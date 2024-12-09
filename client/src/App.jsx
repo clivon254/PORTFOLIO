7f7fffe60615efc44eae9270c65414d21ca81029
Clivon Osire <clivon254@icloud.com>
2024-12-09 08:28:30 +0300

    nime

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,44 @@
 
 
+import { BrowserRouter, Route } from "react-router-dom"
+import {Toaster} from "sonner"
+import SignIn from "./pages/SignIn"
+import ResetPassword from "./pages/ResetPassword"
+import ForgotPassword from "./pages/ForgotPassword"
+
+
 export default function App()
 {
+
+  const LayOut = () => {
+
+    return(
+
+      <div className=""></div>
+
+    )
+
+  }
+
   return (
 
-    <div className="">
+    <BrowserRouter>
 
-      <p className="text-7xl text-red-500 font-black">
-        Hitman mtaa mi ni jitu
-      </p>
+        <Toaster richColors/>
 
-    </div>
+        <main className="min-h-screen">
+        
+         <Route element={LayOut}></Route>
+
+         <Route path="/sign-in" element={<SignIn/>} />
+
+         <Route path="/forgot-password" element={<ForgotPassword/>} />
+
+         <Route path="/reset-password/:token" element={<ResetPassword/>} />
+
+        </main>
+
+    </BrowserRouter>
 
   )
 }
